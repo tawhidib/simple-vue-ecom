196b98fc755fe69051ad7151c27bb9903f4e4b8c
tawhidib <tawhidib@gmail.com>
2025-07-23 20:23:15 +0600

    feat: tailwind added

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,10 @@
 <template>
-	<div></div>
+	<div class="container bg-red-500">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni eaque
+		dignissimos asperiores aliquam commodi at cumque, placeat alias, enim,
+		fugit delectus explicabo! Dolores veritatis officiis rerum similique
+		praesentium. Provident, error.
+	</div>
 </template>
+
+<style></style>
